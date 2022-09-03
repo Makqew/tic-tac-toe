439f555242de99e4ef10c77bbbcae51a59f3f504
Makqew <112494472+Makqew@users.noreply.github.com>
2022-09-04 02:07:01 +0600

    functional implementation

--- main.js
+++ main.js
@@ -1,8 +1,45 @@
-let arr = [];
-const player = (order, name) => {
-    return { order, name };
+let arr = ['-','-','-','-','-','-','-','-','-','-','-','-'];
+const player = (order, name, sign) => {
+    return { order, name, sign };
 };
+let i = 0;
 
-const playerOne = player(1, 'Player 1');
-const playerTwo = player(2, 'Player 2');
-console.log(playerOne.name)
+let nameList = $('.columns');
+const playerOne = player(1, 'Player 1', 'x');
+const playerTwo = player(2, 'Player 2', 'o');
+console.log(playerOne.name);
+
+// if (xxx --- ---) or (--- xxx ---) or (--- --- xxx) 
+// or (x-- -x- --x) or (--x -x- x--)
+// or (x-- x-- x--) or (-x- -x- -x-) or (--x --x --x)
+// every odd number player1 makes a move
+function placeSign(event) {
+    
+    if(i != 9){
+        if(i%2 == 0 ){
+            if(event.currentTarget.innerHTML != 'o' && event.currentTarget.innerHTML != 'x'){
+                event.currentTarget.innerHTML = playerOne.sign;
+                arr.splice(i, 1, playerOne.sign);
+                i++;
+            }
+            
+        } else {
+            if(event.currentTarget.innerHTML != 'x' && event.currentTarget.innerHTML != 'o'){
+                event.currentTarget.innerHTML = playerTwo.sign;
+                arr.splice(i, 1, playerTwo.sign);
+                i++;
+            }
+    }
+        console.log(i);
+        }
+    else {
+        alert('bruh')
+    }
+    console.log(arr)
+    
+}
+
+let cell = $('.column');
+for( let i = 0; i < cell.length; i++){
+    cell[i].addEventListener('click', placeSign);
+}
